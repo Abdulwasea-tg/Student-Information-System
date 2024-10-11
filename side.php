dbfd1a0ca7d7f53cba8ee7811e7dd6b01e0d0f5d
Abdulwasea-tg <abdulwaseaalabduly@gmail.com>
2024-10-12 02:54:33 +0300

    changed all nav icons

--- side.php
+++ side.php
@@ -2,10 +2,10 @@
 echo '<div class="nav">
 <ul>
     <li><a href="index.php"><button id="tab_1" class="btn_tab" href=""><img src="..//assets//img//icon//home.png" alt="noimg"></img>Home</button></a></li>
-    <li><a href="student_list.php"><button id="tab_2" class="btn_tab" href=""><img src="..//assets//img//icon//user (1).png" alt="noimg"></img>Students</button></a></li>
-    <li><a href="department_List.php"><button id="tab_3" class="btn_tab" href=""><img src="..//assets//img//icon//job.png" alt="noimg"></img>Departments</button></a></li>
-    <li><a href="instructor_List.php"><button id="tab_4" class="btn_tab" href=""><img src="..//assets//img//icon//job.png" alt="noimg"></img>Instructors</button></a></li>
-    <li><a href="course_List.php"><button id="tab_5" class="btn_tab" href=""><img src="..//assets//img//icon//application.png" alt="noimg"></img>Courses</button></a></li>
+    <li><a href="student_list.php"><button id="tab_2" class="btn_tab" href=""><img src="..//assets//img//icon//student.png" alt="noimg"></img>Students</button></a></li>
+    <li><a href="department_List.php"><button id="tab_3" class="btn_tab" href=""><img src="..//assets//img//icon//building.png" alt="noimg"></img>Departments</button></a></li>
+    <li><a href="instructor_List.php"><button id="tab_4" class="btn_tab" href=""><img src="..//assets//img//icon//instructor.png" alt="noimg"></img>Instructors</button></a></li>
+    <li><a href="course_List.php"><button id="tab_5" class="btn_tab" href=""><img src="..//assets//img//icon//e-learning.png" alt="noimg"></img>Courses</button></a></li>
     <li><a href="enrollment_List.php"><button id="tab_5" class="btn_tab" href=""><img src="..//assets//img//icon//application.png" alt="noimg"></img>Enrollment</button></a></li>
 
 </ul>
