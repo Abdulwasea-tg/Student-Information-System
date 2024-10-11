a960e51850e768a4cacbc20b3a6e1c460e33dec1
Abdulwasea-tg <abdulwaseaalabduly@gmail.com>
2024-10-12 02:14:52 +0300

    modified header and changed logo

--- layout/header.php
+++ layout/header.php
@@ -1,9 +1,11 @@
-<div class="header">
+<div class="header" style="background: linear-gradient(to right,  rgba(0, 42, 255, 0.492),#4c7caf);">
     <!-- titel -->
-    <div class="logo_box">
-        <a href="../app/index.php"><img class="logo" src="../assets//img//logo//logo.png" alt="noimg"></img></a>  
+    <div class="logo_box" >
+    <h1 style="font-size:24px;color:white;font-weight:900">Student Information System</h1>
+        <!-- <a href="../app/index.php"><img class="logo" src="../assets//img//logo//logo2.png" alt="noimg"></img></a>   -->
+         
     </div>
-
+    
     <div class="logout">
         <a href="../layout/Logout.php"><button class="btn_logout" href="">Logout</button></a>
     </div>
